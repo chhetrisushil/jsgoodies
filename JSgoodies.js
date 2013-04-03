7d1c0f7c6d3e14e46b8b8e1f123cdbde317a97c2
Sushil Chhetri <chhetrisushil@gmail.com>
2013-04-03 13:09:18 +0530

    * Formatting

--- JSgoodies.js
+++ JSgoodies.js
@@ -151,9 +151,9 @@
         __extends = function (prop) {
             var _super = prop;
 			
-	    if (prop instanceof Interface) {
-		throw new TypeError('Class cannot extend interface');
-	    }
+            if (prop instanceof Interface) {
+                throw new TypeError('Class cannot extend interface');
+            }
 			
             if (typeof prop === 'function') {
                 initializing = true;
