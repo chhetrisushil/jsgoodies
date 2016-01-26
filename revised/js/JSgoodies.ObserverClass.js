dffbde9ee31383ca4ce84afef9c912d175753885
Sushil Chhetri <chhetrisushil@gmail.com>
2016-01-27 03:04:18 +0530

    Created Observer Interface

--- revised/js/JSgoodies.ObserverClass.js
+++ revised/js/JSgoodies.ObserverClass.js
@@ -2,10 +2,10 @@
     "use strict";
 
     var ObserverClass = JSgoodies.CreateClass({
-        update: function() {
+        update: function(observable) {
             throw new Error('Implement the Update method');
         }
-    });
+    }).implements(JSgoodies.ObserverInterface);
 
     JSgoodies.ObserverClass = JSgoodies.ObserverClass || ObserverClass;
 })(window, window.JSgoodies);
---
+++ revised/js/JSgoodies.ObserverInterface.js
@@ -0,0 +1,7 @@
+(function(W, JSgoodies) {
+    "use strict";
+
+    var ObserverInterface = JSgoodies.Interface('Observer', ['update']);
+
+    JSgoodies.ObserverInterface = JSgoodies.ObserverInterface || ObserverInterface;
+})(window, window.JSgoodies);
